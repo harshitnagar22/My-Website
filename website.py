262ec6750680efe63fa5fe234692e0b5bdbe4949
Harshit Nagar <156645072+harshitnagar22@users.noreply.github.com>
2025-01-05 01:18:44 +0530

    Add files via upload

---
+++ website.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", layout="wide")
+
+st.title("Hello, I am Harshit")
+
+st.header("About Me")
+st.write("I’m Harshit Nagar, a 19-year-old from Kota, Rajasthan, currently pursuing a B.Tech in Computer Science and Engineering with a focus on Artificial Intelligence and Machine Learning at JECRC University. I’m passionate about leveraging AI to tackle real-world challenges and drive innovation.")
+
+st.write("I have created this website using Python and Streamlit")
+
+st.subheader("Connect with Me :-")
+st.write("[Git Hub](https://github.com/harshitnagar22)")
+
+st.write("[Linkdin](https://www.linkedin.com/in/harshit-nagar-708a27290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app)")
+
+st.write("[Instagram](https://www.instagram.com/harshit.nagar_/profilecard/?igsh=MWEzdW14aW5iN3N3bA%3D%3D)")
